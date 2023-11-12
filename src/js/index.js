790b6e63a98e34a25e7a76c4166288b7cda1ef5b
Margaret <match14@yandex.ru>
2023-11-12 20:21:50 +0300

    learning

--- src/js/index.js
+++ src/js/index.js
@@ -403,3 +403,17 @@ var prodDetailSlider = new Swiper('.js-prod-detail-slider', {
 		swiper: prodDetailSliderThumb,
 	},
 });
+
+
+// Плавный переход к блоку
+if($('.js-link-move').length){
+	$('.js-link-move').on('click', function() {
+		var posBlock = $('.js-to-move[data-move='+$(this).data('move')+']').offset().top;
+
+		$(window).on('resize', function(){
+			posBlock = $('.js-to-move[data-move='+$(this).data('move')+']').offset().top;
+		});
+		
+		$('html, body').animate({ scrollTop: posBlock }, 600);
+	});
+}
